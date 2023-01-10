bf3ba05b8c0bbe0e41c9ada0a8df78574e55f1bf
Khurshid Uzokov <xurshiduzoqov1@gmail.com>
2023-01-10 22:58:57 +0500

    adding new section

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
-const slideGallery = document.querySelector('.carousel-wrap');
-const slides = slideGallery.querySelectorAll('.carousel-cell');
-const thumbnailContainer = document.querySelector('.swiper-pagination');
-const slideCount = slides.length;
-const slideWidth = 540;
-
-const highlightThumbnail = () => {
-  thumbnailContainer
-    .querySelectorAll('div.highlighted')
-    .forEach(el => el.classList.remove('highlighted'));
-  const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-  thumbnailContainer
-    .querySelector(`div[data-id="${index}"]`)
-    .classList.add('highlighted');
-};
-
-const scrollToElement = el => {
-  const index = parseInt(el.dataset.id, 3);
-  slideGallery.scrollTo(index * slideWidth, 0);
-};
-
-thumbnailContainer.innerHTML += [...slides]
-  .map((slide, i) => `<div data-id="${i}"></div>`)
-  .join('');
-
-thumbnailContainer.querySelectorAll('div').forEach(el => {
-  el.addEventListener('click', () => scrollToElement(el));
+var swiper = new Swiper(".main-swiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
 });
 
-slideGallery.addEventListener('scroll', e => highlightThumbnail());
 
-highlightThumbnail();
+// const slideGallery = document.querySelector('.carousel-wrap');
+// const slides = slideGallery.querySelectorAll('.carousel-cell');
+// const thumbnailContainer = document.querySelector('.swiper-pagination');
+// const slideCount = slides.length;
+// const slideWidth = 540;
+
+// const highlightThumbnail = () => {
+//   thumbnailContainer
+//     .querySelectorAll('div.highlighted')
+//     .forEach(el => el.classList.remove('highlighted'));
+//   const index = Math.floor(slideGallery.scrollLeft / slideWidth);
+//   thumbnailContainer
+//     .querySelector(`div[data-id="${index}"]`)
+//     .classList.add('highlighted');
+// };
+
+// const scrollToElement = el => {
+//   const index = parseInt(el.dataset.id, 3);
+//   slideGallery.scrollTo(index * slideWidth, 0);
+// };
+
+// thumbnailContainer.innerHTML += [...slides]
+//   .map((slide, i) => `<div data-id="${i}"></div>`)
+//   .join('');
+
+// thumbnailContainer.querySelectorAll('div').forEach(el => {
+//   el.addEventListener('click', () => scrollToElement(el));
+// });
+
+// slideGallery.addEventListener('scroll', e => highlightThumbnail());
+
+// highlightThumbnail();
