b872ef927c3decdd516af20c3499b81203111ccc
Khurshid Uzokov <xurshiduzoqov1@gmail.com>
2023-01-11 00:31:03 +0500

    changing all section

--- js/script.js
+++ js/script.js
@@ -1,47 +1,24 @@
-var swiper = new Swiper(".main-swiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-});
+function burgerMenu() {
+    const burgerList = document.querySelector('.burger-menu-list');
+    const burger = document.querySelector('.burger');
+    const burgerLink = document.querySelectorAll('.burger-menu-list ul li a');
 
 
-// const slideGallery = document.querySelector('.carousel-wrap');
-// const slides = slideGallery.querySelectorAll('.carousel-cell');
-// const thumbnailContainer = document.querySelector('.swiper-pagination');
-// const slideCount = slides.length;
-// const slideWidth = 540;
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        burgerList.classList.toggle('active');
+    });
 
-// const highlightThumbnail = () => {
-//   thumbnailContainer
-//     .querySelectorAll('div.highlighted')
-//     .forEach(el => el.classList.remove('highlighted'));
-//   const index = Math.floor(slideGallery.scrollLeft / slideWidth);
-//   thumbnailContainer
-//     .querySelector(`div[data-id="${index}"]`)
-//     .classList.add('highlighted');
-// };
+    burgerLink.forEach((item) => {
+        item.addEventListener('click', () => {
+            burger.classList.toggle('active');
+            burgerList.classList.toggle('active');
+        })
+    })
 
-// const scrollToElement = el => {
-//   const index = parseInt(el.dataset.id, 3);
-//   slideGallery.scrollTo(index * slideWidth, 0);
-// };
+}
+burgerMenu();
 
-// thumbnailContainer.innerHTML += [...slides]
-//   .map((slide, i) => `<div data-id="${i}"></div>`)
-//   .join('');
 
-// thumbnailContainer.querySelectorAll('div').forEach(el => {
-//   el.addEventListener('click', () => scrollToElement(el));
-// });
 
-// slideGallery.addEventListener('scroll', e => highlightThumbnail());
 
-// highlightThumbnail();
